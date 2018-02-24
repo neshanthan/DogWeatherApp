66361b7f53688417f666f5dfd1c15d72756c5820
Ian Zakharov <i.zakharov@se16.qmul.ac.uk>
2018-02-24 19:03:08 +0000

    Got intellij(A java IDE) to work with react, which wasn't suppose to work. Все будет хорошо Каамил ;)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to Toms White Only club</h1>
+                    <h2><center>Ian made intellij work!</center></h2>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
